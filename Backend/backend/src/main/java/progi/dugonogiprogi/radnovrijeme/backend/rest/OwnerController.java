be7b0b9ac9b4ab778c4a3c3b4f72967b7f1834bc
barb.pavl <bp52606@fer.hr>
2021-11-10 10:15:40 +0100

    Dodan OwnerController, OwnerService i OwnerServiceJpa

---
+++ Backend/backend/src/main/java/progi/dugonogiprogi/radnovrijeme/backend/rest/OwnerController.java
@@ -0,0 +1,19 @@
+package progi.dugonogiprogi.radnovrijeme.backend.rest;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+
+@RestController
+public class OwnerController {
+
+
+    public void createGroup() {
+
+    }
+
+
+
+
+
+}
