eccace2de020d85fcff67a05025817a28f73e7f9
ante pasalic <ante.pv5@gmail.com>
2021-11-16 00:34:52 +0100

    MoneyManagement edit, fetch attempt

--- frontend/ui/src/App.js
+++ frontend/ui/src/App.js
@@ -1,24 +1,26 @@
-import logo from './logo.svg';
+
 import './App.css';
+import {BrowserRouter as Router,Routes, Route} from 'react-router-dom';
+import JobCont from './components/JobCont';
+import GroupCont from './components/GroupCont';
+import MoneyMng from './components/MoneyMng';
+
 
 function App() {
+  
+  
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <Route path="/groupcont">
+        <GroupCont/>
+      </Route>
+      <Route path="/jobcont">
+        <JobCont/>
+      </Route>
+      <Route path="/moneymng">
+        <MoneyMng/>
+      </Route>
+    </Router>
   );
 }
 
--- frontend/ui/src/components/MoneyMng.js
+++ frontend/ui/src/components/MoneyMng.js
@@ -1,18 +1,35 @@
 import React from 'react'
+import { useState, useEffect } from 'react';
 
-const MoneyMng = async () => {
+const MoneyMng =  ()  => {
+    const [profits, setProfits] = useState([]);
+
     const fetchInfo = async () => {
-        const res = await fetch('http://localhost:8080/moneymanagement')
+        console.log("TU SAAAAAM")
+        const res = await fetch('http://localhost:8080/moneymanagement') //.then((response) => setProfits(response.json))
         const data = await res.json()
-    
         return data
+        // //setProfits(data)
+        // console.log(data)
+        //return fetch("http://localhost:8080/moneymanagement").then((response) => response.json()).then((data) => console.log(data));
     }
+
+    useEffect(() => {
+        const getInfo = async () => {
+          const tasksFromServer = await fetchInfo()
+          setProfits(tasksFromServer)
+        }
+    
+        getInfo()
+    }, [])
 
     return (
         <div>
             <h1>Money management</h1>
+            <p>{fetchInfo}</p> 
+
         </div>
-    )
+    );
 }
 
-export default MoneyMng
+export default MoneyMng;
