25941a0332aa9efcdb394f6303ec8f086e766650
BernardKazazic <bernard.kazazic@fer.hr>
2021-11-15 22:41:22 +0100

    WebSecurityConfig preraden.

--- Backend/backend/src/main/java/progi/dugonogiprogi/radnovrijeme/backend/rest/EmployeeUserDetailsService.java
+++ Backend/backend/src/main/java/progi/dugonogiprogi/radnovrijeme/backend/rest/EmployeeUserDetailsService.java
@@ -17,14 +17,9 @@ public class EmployeeUserDetailsService implements UserDetailsService {
     @Autowired
     EmployeeRepository employeeRepository;
 
-    @Autowired
-    PasswordEncoder passwordEncoder;
-
     @Override
     public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
-        Employee user = null;
-        if(employeeRepository.findByUsername(username).isPresent())
-            user = employeeRepository.findByUsername(username).get();
+        Employee user = employeeRepository.findByUsername(username).get();
         if(user == null)
             throw new UsernameNotFoundException("User with name: " + username + " does not exist.");
         return new EmployeeUserDetailsImpl(user);
--- Backend/backend/src/main/java/progi/dugonogiprogi/radnovrijeme/backend/service/impl/TaskServiceJpa.java
+++ Backend/backend/src/main/java/progi/dugonogiprogi/radnovrijeme/backend/service/impl/TaskServiceJpa.java
@@ -15,7 +15,7 @@ import java.util.List;
 import java.util.Set;
 
 @Service
-public class TaskServiceImpl implements TaskService {
+public class TaskServiceJpa implements TaskService {
 
     @Autowired
     EmployeeRepository employeeRepository;
