f5194b34ba052e9c340dcb614cece1f7511e16bd
marijan.erlic <me52218@fer.hr>
2021-11-15 22:56:15 +0100

    dodana ograničenja pri kreiranju djelatnosti

--- Backend/backend/src/main/java/progi/dugonogiprogi/radnovrijeme/backend/domain/Job.java
+++ Backend/backend/src/main/java/progi/dugonogiprogi/radnovrijeme/backend/domain/Job.java
@@ -1,9 +1,6 @@
 package progi.dugonogiprogi.radnovrijeme.backend.domain;
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.Id;
-import javax.persistence.OneToMany;
+import javax.persistence.*;
 import javax.validation.constraints.NotNull;
 import java.util.Set;
 
@@ -27,6 +24,7 @@ public class Job {
 	/**
 	 * Job name.
 	 */
+	@Column(unique = true)
 	@NotNull
 	private String name;
 
--- Backend/backend/src/main/java/progi/dugonogiprogi/radnovrijeme/backend/service/impl/JobServiceJpa.java
+++ Backend/backend/src/main/java/progi/dugonogiprogi/radnovrijeme/backend/service/impl/JobServiceJpa.java
@@ -2,6 +2,7 @@ package progi.dugonogiprogi.radnovrijeme.backend.service.impl;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.util.Assert;
 import progi.dugonogiprogi.radnovrijeme.backend.dao.JobRepository;
 import progi.dugonogiprogi.radnovrijeme.backend.domain.Job;
 import progi.dugonogiprogi.radnovrijeme.backend.service.JobService;
@@ -46,6 +47,10 @@ public class JobServiceJpa implements JobService {
      */
     @Override
     public Job createJob(String jobName, String jobDescription) {
+        Assert.notNull(jobName, "Job name must be given!");
+        Assert.notNull(jobDescription, "Job decription must be given!");
+        if (jobRepo.countByName(jobName) > 0)
+            throw new RequestDeniedException("A job with the name " + jobName + "already exists");
         return jobRepo.save(new Job(jobName, jobDescription));
     }
 }
