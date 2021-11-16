99dcc4e43c699382c89533e60a0465364764eb8f
LforLuka <luka.raspudic@fer.hr>
2021-11-16 11:54:45 +0100

    dodan dio forme za render

--- frontend/ui/src/components/Jobs.js
+++ frontend/ui/src/components/Jobs.js
@@ -2,9 +2,11 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import '../Jobs.css';
 const Jobs = () => {
+
     return (
         <form>
             <label>Jobs page</label>
+            <input type="text" name="employeeID"></input>
         </form>
     )
 };
