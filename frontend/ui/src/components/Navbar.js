e3eaa85116bfb26c27fd0f89e69e9e02f065e032
Petar Susac <petar.susac@gmail.com>
2021-12-13 19:51:34 +0100

    Manji fix

--- frontend/ui/src/components/Navbar.js
+++ frontend/ui/src/components/Navbar.js
@@ -19,7 +19,7 @@ function Navbar() {
     <Router>
       <nav className="navbar navbar-expand-lg navbar-light bg-light">
         <div className="container-fluid justify-content-around">
-        <a class="navbar-brand" href="#">Navbar</a>
+        <a class="navbar-brand" href="#">Radno vrijeme</a>
           <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
             <span className="navbar-toggler-icon"></span>
           </button>
