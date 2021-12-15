870ef60fbdbc639f66040ebc158468565e6e5737
BernardKazazic <bernard.kazazic@fer.hr>
2021-12-15 17:39:26 +0100

    Popravljen job controller.

--- Backend/backend/src/main/java/progi/dugonogiprogi/radnovrijeme/backend/rest/RegistrationController.java
+++ Backend/backend/src/main/java/progi/dugonogiprogi/radnovrijeme/backend/rest/RegistrationController.java
@@ -6,6 +6,7 @@ import org.springframework.web.bind.annotation.*;
 import progi.dugonogiprogi.radnovrijeme.backend.rest.dto.RegistrationDTO;
 import progi.dugonogiprogi.radnovrijeme.backend.service.RegistrationService;
 
+@CrossOrigin("*")
 @RestController
 @RequestMapping("/register")
 public class RegistrationController {
