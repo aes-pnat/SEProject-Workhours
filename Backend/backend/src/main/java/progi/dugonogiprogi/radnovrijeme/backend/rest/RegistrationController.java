aad87c54fa00a481f626cfa2593b1a75bbccbeb0
BernardKazazic <bernard.kazazic@fer.hr>
2021-12-15 18:57:55 +0100

    Popravljen return request

--- Backend/backend/src/main/java/progi/dugonogiprogi/radnovrijeme/backend/rest/RegistrationController.java
+++ Backend/backend/src/main/java/progi/dugonogiprogi/radnovrijeme/backend/rest/RegistrationController.java
@@ -2,9 +2,15 @@ package progi.dugonogiprogi.radnovrijeme.backend.rest;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 import progi.dugonogiprogi.radnovrijeme.backend.rest.dto.RegistrationDTO;
 import progi.dugonogiprogi.radnovrijeme.backend.service.RegistrationService;
+
+import javax.validation.Valid;
+import java.net.URI;
 
 @CrossOrigin("*")
 @RestController
@@ -15,7 +21,8 @@ public class RegistrationController {
     RegistrationService registrationService;
 
     @PostMapping("")
-    public void registerEmployee(@RequestBody RegistrationDTO regData) {
-        registrationService.registerEmployee(regData);
+    public ResponseEntity<?> registerEmployee(@RequestBody RegistrationDTO regData) {
+        URI uri = URI.create(ServletUriComponentsBuilder.fromCurrentContextPath().path("/register").toUriString());
+        return ResponseEntity.created(uri).body(registrationService.registerEmployee(regData));
     }
 }
--- Backend/backend/src/main/java/progi/dugonogiprogi/radnovrijeme/backend/rest/dto/RegistrationDTO.java
+++ Backend/backend/src/main/java/progi/dugonogiprogi/radnovrijeme/backend/rest/dto/RegistrationDTO.java
@@ -1,19 +1,37 @@
 package progi.dugonogiprogi.radnovrijeme.backend.rest.dto;
 
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.Size;
+
 public class RegistrationDTO {
 
+    @NotEmpty(message = "Pid should not be empty.")
+    @Size(min = 11, max = 11, message = "Pid should be 11 characters long.")
     private String pid;
 
+    @NotEmpty(message = "Name should not be empty.")
+    @NotBlank(message = "Name should not be blank.")
     private String name;
 
+    @NotEmpty(message = "Surname should not be empty.")
+    @NotBlank(message = "Surname should not be blank.")
     private String surname;
 
+    @NotEmpty(message = "Email should not be empty.")
+    @NotBlank(message = "Email should not be blank.")
     private String email;
 
+    @NotEmpty(message = "Username should not be empty.")
+    @NotBlank(message = "Username should not be blank.")
     private String username;
 
+    @NotEmpty(message = "Password should not be empty.")
+    @NotBlank(message = "Password should not be blank.")
     private String password;
 
+    @NotEmpty(message = "Password check should not be empty.")
+    @NotBlank(message = "Password check should not be blank.")
     private String passwordCheck;
 
     public String getPid() {
