ab6e9525395061f09f984dda908c7f013bdf11c5
marijan.erlic <me52218@fer.hr>
2021-12-16 17:38:53 +0100

    Dodani OccupancyController, EmployeeDTO, OccupancyService, OccupancyServiceJpa

---
+++ Backend/backend/src/main/java/progi/dugonogiprogi/radnovrijeme/backend/service/OccupancyService.java
@@ -0,0 +1,17 @@
+package progi.dugonogiprogi.radnovrijeme.backend.service;
+
+import org.springframework.stereotype.Service;
+import progi.dugonogiprogi.radnovrijeme.backend.rest.dto.EmployeeDTO;
+
+import java.time.Instant;
+import java.util.Date;
+import java.util.List;
+
+@Service
+public interface OccupancyService {
+
+    List<EmployeeDTO> listAllEmployees();
+
+    String isOccupied(String id, Date dateStart, Date dateEnd);
+
+}
