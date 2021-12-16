b6e998338ead14de4212b33dfda291df8d054253
barb.pavl <bp52606@fer.hr>
2021-12-16 22:55:53 +0100

    Popravljene greške u kodu

--- Backend/backend/src/main/java/progi/dugonogiprogi/radnovrijeme/backend/dao/EmployeegroupRepository.java
+++ Backend/backend/src/main/java/progi/dugonogiprogi/radnovrijeme/backend/dao/EmployeegroupRepository.java
@@ -1,6 +1,5 @@
 package progi.dugonogiprogi.radnovrijeme.backend.dao;
 
-<<<<<<< HEAD
 import org.springframework.stereotype.Repository;
 import progi.dugonogiprogi.radnovrijeme.backend.domain.Employeegroup;
 
@@ -14,11 +13,4 @@ public interface EmployeegroupRepository {
 
     Optional<List<Employeegroup>> findById_Idgroup(Integer idgroup);
 }
-=======
-import org.springframework.data.jpa.repository.JpaRepository;
-import progi.dugonogiprogi.radnovrijeme.backend.domain.Employeegroup;
-import progi.dugonogiprogi.radnovrijeme.backend.domain.EmployeegroupId;
 
-public interface EmployeegroupRepository extends JpaRepository<Employeegroup, EmployeegroupId> {
-}
->>>>>>> c5e3810d174aa1d613d83ea64421cb9f32a0d1b3
