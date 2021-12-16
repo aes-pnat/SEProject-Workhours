5a8a3889753eebc4356cb60966f7af68c6829157
BernardKazazic <bernard.kazazic@fer.hr>
2021-12-17 00:21:58 +0100

    Dovrsen authorization filter. Security implementiran.

--- Backend/backend/src/main/java/progi/dugonogiprogi/radnovrijeme/backend/rest/security/SecurityConfig.java
+++ Backend/backend/src/main/java/progi/dugonogiprogi/radnovrijeme/backend/rest/security/SecurityConfig.java
@@ -9,7 +9,9 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 import progi.dugonogiprogi.radnovrijeme.backend.rest.security.filter.CustomAuthenticationFilter;
+import progi.dugonogiprogi.radnovrijeme.backend.rest.security.filter.CustomAuthorizationFilter;
 
 import static org.springframework.security.config.http.SessionCreationPolicy.STATELESS;
 
@@ -32,8 +34,10 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
     protected void configure(HttpSecurity http) throws Exception {
         http.csrf().disable();
         http.sessionManagement().sessionCreationPolicy(STATELESS);
+        //http.authorizeRequests().antMatchers("/login").permitAll();
         http.authorizeRequests().anyRequest().permitAll();
         http.addFilter(new CustomAuthenticationFilter(authenticationManagerBean()));
+        http.addFilterBefore(new CustomAuthorizationFilter(), UsernamePasswordAuthenticationFilter.class);
     }
 
     @Bean
--- Backend/backend/src/main/java/progi/dugonogiprogi/radnovrijeme/backend/rest/security/filter/CustomAuthorizationFilter.java
+++ Backend/backend/src/main/java/progi/dugonogiprogi/radnovrijeme/backend/rest/security/filter/CustomAuthorizationFilter.java
@@ -1,7 +1,15 @@
 package progi.dugonogiprogi.radnovrijeme.backend.rest.security.filter;
 
+import com.auth0.jwt.JWT;
+import com.auth0.jwt.JWTVerifier;
 import com.auth0.jwt.algorithms.Algorithm;
+import com.auth0.jwt.interfaces.DecodedJWT;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.filter.OncePerRequestFilter;
 
 import javax.servlet.FilterChain;
@@ -9,9 +17,16 @@ import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.HashMap;
+import java.util.Map;
 
+import static java.util.Arrays.stream;
 import static org.springframework.http.HttpHeaders.AUTHORIZATION;
+import static org.springframework.http.MediaType.APPLICATION_JSON_VALUE;
 
+@Slf4j
 public class CustomAuthorizationFilter extends OncePerRequestFilter {
 
     @Override
@@ -22,8 +37,32 @@ public class CustomAuthorizationFilter extends OncePerRequestFilter {
         else {
             String authorizationHeader = request.getHeader(AUTHORIZATION);
             if(authorizationHeader != null && authorizationHeader.startsWith("Bearer ")) {
-                String token = authorizationHeader.substring("Bearer ".length());
-                Algorithm algorithm = Algorithm.HMAC256("secret".getBytes());
+                try{
+                    String token = authorizationHeader.substring("Bearer ".length());
+                    Algorithm algorithm = Algorithm.HMAC256("secret".getBytes());
+                    JWTVerifier verifier = JWT.require(algorithm).build();
+                    DecodedJWT decodedJWT = verifier.verify(token);
+                    String username = decodedJWT.getSubject();
+                    String[] roles = decodedJWT.getClaim("roles").asArray(String.class);
+                    Collection<SimpleGrantedAuthority> authorities = new ArrayList<>();
+                    stream(roles).forEach(role -> authorities.add(new SimpleGrantedAuthority(role)));
+                    UsernamePasswordAuthenticationToken authenticationToken = new UsernamePasswordAuthenticationToken(username, null, authorities);
+                    SecurityContextHolder.getContext().setAuthentication(authenticationToken);
+                    filterChain.doFilter(request, response);
+                }
+                catch(Exception e) {
+                    log.error("Error logging in: {}", e.getMessage());
+                    response.setStatus(403);
+                    Map<String, String> props = new HashMap<>();
+                    props.put("message", e.getMessage());
+                    props.put("status", "403");
+                    props.put("error", "Forbidden");
+                    response.setContentType(APPLICATION_JSON_VALUE);
+                    new ObjectMapper().writeValue(response.getOutputStream(), props);
+                }
+            }
+            else {
+                filterChain.doFilter(request, response);
             }
         }
     }
