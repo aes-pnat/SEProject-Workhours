a14bbea846b2c947f40088863cae4fc042e29229
BernardKazazic <bernard.kazazic@fer.hr>
2021-12-16 23:24:45 +0100

    Dodan custom authorization filter.

---
+++ Backend/backend/src/main/java/progi/dugonogiprogi/radnovrijeme/backend/rest/security/filter/CustomAuthorizationFilter.java
@@ -0,0 +1,30 @@
+package progi.dugonogiprogi.radnovrijeme.backend.rest.security.filter;
+
+import com.auth0.jwt.algorithms.Algorithm;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import javax.servlet.FilterChain;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+import static org.springframework.http.HttpHeaders.AUTHORIZATION;
+
+public class CustomAuthorizationFilter extends OncePerRequestFilter {
+
+    @Override
+    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
+        if(request.getServletPath().equals("/login")) {
+            filterChain.doFilter(request, response);
+        }
+        else {
+            String authorizationHeader = request.getHeader(AUTHORIZATION);
+            if(authorizationHeader != null && authorizationHeader.startsWith("Bearer ")) {
+                String token = authorizationHeader.substring("Bearer ".length());
+                Algorithm algorithm = Algorithm.HMAC256("secret".getBytes());
+            }
+        }
+    }
+}
