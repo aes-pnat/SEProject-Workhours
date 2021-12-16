d21ec1055034838e5afaf477690d141f377aa71d
BernardKazazic <bernard.kazazic@fer.hr>
2021-12-16 17:55:28 +0100

    Stvoren custom authentication filter i doraden security config.

--- Backend/backend/src/main/java/progi/dugonogiprogi/radnovrijeme/backend/rest/security/SecurityConfig.java
+++ Backend/backend/src/main/java/progi/dugonogiprogi/radnovrijeme/backend/rest/security/SecurityConfig.java
@@ -8,6 +8,8 @@ import org.springframework.security.config.annotation.web.configuration.EnableWe
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
+import static org.springframework.security.config.http.SessionCreationPolicy.STATELESS;
+
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
@@ -25,6 +27,9 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
-        super.configure(http);
+        http.csrf().disable();
+        http.sessionManagement().sessionCreationPolicy(STATELESS);
+        http.authorizeRequests().anyRequest().permitAll();
+        http.addFilter(null);
     }
 }
