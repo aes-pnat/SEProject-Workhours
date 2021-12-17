c452466cfe3be3e185419c23cca3ac80a3d353bc
marijan.erlic <me52218@fer.hr>
2021-12-17 16:21:04 +0100

    ispravljanje koda

--- Backend/backend/src/main/java/progi/dugonogiprogi/radnovrijeme/backend/rest/MapController.java
+++ Backend/backend/src/main/java/progi/dugonogiprogi/radnovrijeme/backend/rest/MapController.java
@@ -2,6 +2,7 @@ package progi.dugonogiprogi.radnovrijeme.backend.rest;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import progi.dugonogiprogi.radnovrijeme.backend.rest.dto.LocationDataDTO;
@@ -17,7 +18,7 @@ public class MapController {
     @Autowired
     MapService mapService;
 
-
+    @GetMapping("")
     public List<LocationDataDTO> showLocationData() {
         return mapService.showLocationData();
     }
--- Backend/backend/src/main/java/progi/dugonogiprogi/radnovrijeme/backend/rest/OccupancyController.java
+++ Backend/backend/src/main/java/progi/dugonogiprogi/radnovrijeme/backend/rest/OccupancyController.java
@@ -6,9 +6,12 @@ import org.springframework.web.bind.annotation.*;
 import progi.dugonogiprogi.radnovrijeme.backend.rest.dto.EmployeeDTO;
 import progi.dugonogiprogi.radnovrijeme.backend.service.OccupancyService;
 
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
 import java.time.Instant;
 import java.util.Date;
 import java.util.List;
+import java.util.Locale;
 
 @CrossOrigin("*")
 @RestController
@@ -24,8 +27,9 @@ public class OccupancyController {
     }
 
     @PostMapping("")
-    public String isOccupied(@RequestParam String id, @RequestParam Date dateStart, @RequestParam Date dateEnd) {
-        return occupancyService.isOccupied(id, dateStart, dateEnd);
+    public String isOccupied(@RequestParam String id, @RequestParam String dateStart, @RequestParam String dateEnd) throws ParseException {
+        SimpleDateFormat formatter = new SimpleDateFormat("yyyy-MM-dd", Locale.ENGLISH);
+        return occupancyService.isOccupied(id, formatter.parse(dateStart), formatter.parse(dateEnd));
     }
 
 }
