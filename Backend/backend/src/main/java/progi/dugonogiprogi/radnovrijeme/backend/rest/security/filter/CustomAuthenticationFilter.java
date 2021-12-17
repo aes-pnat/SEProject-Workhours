4e45701f7a9f1d8aeb1c93fd9d0483d97313c467
BernardKazazic <bernard.kazazic@fer.hr>
2021-12-17 21:30:50 +0100

    Username i password se sada primaju iz tijela zahtjeva

--- Backend/backend/src/main/java/progi/dugonogiprogi/radnovrijeme/backend/rest/security/filter/CustomAuthenticationFilter.java
+++ Backend/backend/src/main/java/progi/dugonogiprogi/radnovrijeme/backend/rest/security/filter/CustomAuthenticationFilter.java
@@ -5,7 +5,6 @@ import com.auth0.jwt.algorithms.Algorithm;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import lombok.SneakyThrows;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
