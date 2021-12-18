a8f7aff87454b2bd050e87ef6aa806dc094b28cd
Petar Susac <petar.susac@gmail.com>
2021-12-18 20:23:26 +0100

    Fetch za WorkHoursInput

--- frontend/ui/src/components/WorkHoursInput.js
+++ frontend/ui/src/components/WorkHoursInput.js
@@ -2,10 +2,30 @@ import React from 'react';
 
 class WorkHoursInput extends React.Component {
     state = {
+        HARDKODIRANI_ID_PROMIJENITI_OVO: '00000000001',
         task: '',
         date: '',
         hoursDone: '',
-        message: ''
+        success: null,
+        tasksList: []
+    }
+
+    componentDidMount = async () => {
+        const myHeaders = new Headers();
+		myHeaders.append("Content-Type","application/json");
+        myHeaders.append("Accept","application/json");
+
+        await fetch(process.env.REACT_APP_BACKEND_URL + '/workhoursinput?idEmployee='
+            + this.state.HARDKODIRANI_ID_PROMIJENITI_OVO, {
+            method: 'GET',
+            headers: myHeaders
+        }).then((response) => {
+            if(response.ok) {
+                return response.json();
+            }
+        }).then((jsonResponse) => {
+            this.setState({ tasksList: jsonResponse });
+        })
     }
 
     handleChange = (e) => {
@@ -14,11 +34,59 @@ class WorkHoursInput extends React.Component {
         this.setState({ [name]: value });
     }
 
+    handleSubmit = async (e) => {
+        e.preventDefault();
+        const body = JSON.stringify({
+            task: this.state.task,
+            date: this.state.date,
+            hoursDone: this.state.hoursDone,
+            idEmployee: this.state.HARDKODIRANI_ID_PROMIJENITI_OVO
+        });
+
+        const myHeaders = new Headers();
+		myHeaders.append("Content-Type","application/json");
+        myHeaders.append("Accept","application/json");
+
+        await fetch(process.env.REACT_APP_BACKEND_URL + '/workhoursinput', {
+            method: 'POST',
+            headers: myHeaders,
+            body: body
+        }).then((response) => {
+            if(response.ok){
+                this.setState({ success: true });
+            }
+        }).catch((err) => {
+            throw err;
+        });
+    }
+
     render () {
+        let tasks;
+        if (this.state.tasksList.length === 0) {
+            tasks = (
+                <option disabled>Nema dostupnih zadataka za djelatnika</option>
+            );
+        } else {
+            tasks = this.state.tasksList.map(task => {
+                return (
+                    <option key={task.name} value={task.name}>{task.name}</option>
+                );
+            })
+        }
+
+        let messageBox = '';
+        if (this.state.success) {
+            messageBox = (
+                <div className="alert alert-success">
+                    Radni sati uspješno upisani.
+                </div>
+            );
+        }
+
         return (
             <div className="container mt-5">
                 <div className="container">
-                    {this.state.message}
+                    {messageBox}
                 </div>
                 <div className="row">
                     <form onSubmit={this.handleSubmit}>
@@ -28,8 +96,8 @@ class WorkHoursInput extends React.Component {
                                     name="task"
                                     onChange={this.handleChange}
                             >
-                                <option value="1">Zadatak 1</option>
-                                <option value="2">Zadatak 2</option>
+                                <option selected disabled>Odaberite zadatak</option>
+                                {tasks}
                             </select>
                         </div>
                         <div className='mb-3'>
@@ -52,7 +120,13 @@ class WorkHoursInput extends React.Component {
                                 max="24"
                             />
                         </div>
-                        <button type="submit" className="btn btn-primary mb-5">Pošalji</button>
+                        <button 
+                            type="submit"
+                            className="btn btn-primary mb-5"
+                            onClick={this.handleSubmit}
+                        >
+                            Pošalji
+                        </button>
                     </form>
                 </div>
             </div>
