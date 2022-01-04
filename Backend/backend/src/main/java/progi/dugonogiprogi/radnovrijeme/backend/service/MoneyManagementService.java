a85107ce0a1ed70a7741176acb93a5a272cfe157
barb.pavl <bp52606@fer.hr>
2022-01-04 18:59:49 +0100

    Napisane metode MoneyManagementController, MoneyManagementDTO, MoneyManagementService i MoneyManagementServiceJpa.

--- Backend/backend/src/main/java/progi/dugonogiprogi/radnovrijeme/backend/service/MoneyManagementService.java
+++ Backend/backend/src/main/java/progi/dugonogiprogi/radnovrijeme/backend/service/MoneyManagementService.java
@@ -1,9 +1,13 @@
 package progi.dugonogiprogi.radnovrijeme.backend.service;
 
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 import progi.dugonogiprogi.radnovrijeme.backend.rest.dto.MoneyManagementDTO;
 
 @Service
 public interface MoneyManagementService {
 
+    MoneyManagementDTO seeExpense(double price);
+
+    MoneyManagementDTO seeProfit(double price);
 }
