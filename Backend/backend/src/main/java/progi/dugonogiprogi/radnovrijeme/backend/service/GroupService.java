b9a5eca9a818a3f8a6648c6710dab18434b9309b
BernardKazazic <bernard.kazazic@fer.hr>
2022-01-04 20:51:11 +0100

    Promjenjen GroupController, GroupService i GroupServiceJpa tako da rade sa AddGroupDTO.

--- Backend/backend/src/main/java/progi/dugonogiprogi/radnovrijeme/backend/service/GroupService.java
+++ Backend/backend/src/main/java/progi/dugonogiprogi/radnovrijeme/backend/service/GroupService.java
@@ -2,13 +2,11 @@ package progi.dugonogiprogi.radnovrijeme.backend.service;
 
 
 import org.springframework.stereotype.Service;
-import progi.dugonogiprogi.radnovrijeme.backend.domain.Employee;
 import progi.dugonogiprogi.radnovrijeme.backend.domain.Group;
+import progi.dugonogiprogi.radnovrijeme.backend.rest.dto.AddGroupDTO;
 import progi.dugonogiprogi.radnovrijeme.backend.rest.dto.GroupDTO;
 
 import java.util.List;
-import java.util.Map;
-import java.util.Optional;
 
 /**
  * Specifies how certain business functionalities should be provided.
@@ -16,10 +14,10 @@ import java.util.Optional;
 @Service
 public interface GroupService {
 
-    public List<GroupDTO> listAllGroups();
+    List<GroupDTO> listAllGroups();
 
-    public Group createGroup(GroupDTO group);
+    Group createGroup(AddGroupDTO group);
 
-    public Integer deleteGroup(Integer groupId);
+    Integer deleteGroup(Integer groupId);
 
 }
