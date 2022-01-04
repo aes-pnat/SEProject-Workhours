a5cae273061e5839f68d0835c6eaef1c7a512a12
barb.pavl <bp52606@fer.hr>
2022-01-04 19:18:13 +0100

    Napisan javadoc za MoneyManagementDTO.

--- Backend/backend/src/main/java/progi/dugonogiprogi/radnovrijeme/backend/rest/dto/MoneyManagementDTO.java
+++ Backend/backend/src/main/java/progi/dugonogiprogi/radnovrijeme/backend/rest/dto/MoneyManagementDTO.java
@@ -9,21 +9,47 @@ import java.util.List;
  */
 public class MoneyManagementDTO {
 
+    /**
+     * Total amount of expense/profit.
+     */
     private double price;
+    /**
+     * Calculated difference between expected and real expense/profit.
+     */
     private double difference;
 
+    /**
+     * Getter for amount of expense/profit.
+     *
+     * @return amount of money for expense/profit
+     */
     public double getPrice() {
         return price;
     }
 
+    /**
+     * Setter for amount of expense/profit.
+     *
+     * @param price amount of money for expense/profit
+     */
     public void setPrice(double price) {
         this.price = price;
     }
 
+    /**
+     * Getter for difference between expected and real expense/profit.
+     *
+     * @return difference between expected and real expense/profit
+     */
     public double getDifference() {
         return difference;
     }
 
+    /**
+     * Setter for difference between expected and real expense/profit.
+     *
+     * @param difference difference between expected and real expense/profit
+     */
     public void setDifference(double difference) {
         this.difference = difference;
     }
