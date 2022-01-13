d2daa8e1d9fb270f22c5965097dabe0c949ff8ee
ante.pv5 <ap52554@fer.hr>
2022-01-14 00:09:03 +0100

    main page video start

--- frontend/ui/src/components/MainPage.js
+++ frontend/ui/src/components/MainPage.js
@@ -1,28 +1,17 @@
 import React from 'react'
 import * as VFX from 'react-vfx';
+import Backvid from './video/background.mp4';
 
 const MainPage = () => {
     return (
         <div>
-            <div class="align-top"></div>
-            <div class="align-middle">
-                <p class="text-center h1">Radno vrijeme&copy; by Mi puno radimo </p>
+            <div className="align-top"></div>
+            <div className="align-middle">
+                <p className="text-center h1">Radno vrijeme&copy; by Mi puno radimo </p>
             </div>
-            <VFX.VFXProvider>
-                {/* Render text as image, then apply the shader effect! */}
-                <VFX.VFXSpan shader="rainbow">Hi there!</VFX.VFXSpan>
-
-                {/* Render image with shader */}
-                <VFX.VFXImg src="cat.png" alt="image" shader="rgbShift"/>
-
-                {/* It also supports animated GIFs! */}
-                <VFX.VFXImg src="doge.gif" shader="pixelate"/>
-
-                {/* and videos! */}
-                <VFX.VFXVideo src="mind_blown.mp4"
-                    autoplay playsinline loop muted
-                    shader="halftone"/>
-            </VFX.VFXProvider>
+            <video autoPlay loop muted>
+                <source src={Backvid} type="video/mp4"/>
+            </video>
         </div>
     )
 }
