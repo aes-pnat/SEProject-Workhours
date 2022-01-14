5f6741bac34fb1a91828aad94e045e0b24ec5b87
Petar Susac <petar.susac@gmail.com>
2022-01-14 22:01:58 +0100

    Vracen video

--- frontend/ui/src/App.js
+++ frontend/ui/src/App.js
@@ -6,14 +6,14 @@ import Home from "./components/Home";
 import './App.css';
 import User from './services/User';
 import Navbar from './components/Navbar';
-// import Backvid from './components/video/background.mp4';
+import Backvid from './components/video/background.mp4';
 function App() {
     const [token, setToken] = useState(User.getToken());
     const [role, setRole] = useState(User.getRole());
     const [user, setUser] = useState('');
     return (
         <div>
-            {/*
+            
             <video autoPlay loop muted
             style={{
                 position:"fixed",
@@ -28,7 +28,7 @@ function App() {
             >
                  <source src={Backvid} type="video/mp4"/>
             </video>
-            */}
+            
             <Navbar token={token} 
                 setToken={setToken} 
                 role={role} setRole={setRole} 
