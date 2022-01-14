aabf94147e036383a548ec35e33a37ad863b7b00
ante.pv5 <ap52554@fer.hr>
2022-01-14 01:30:55 +0100

    video overflow

--- frontend/ui/src/components/Groups.js
+++ frontend/ui/src/components/Groups.js
@@ -119,32 +119,38 @@ const Groups = (props) => {
         {props.role === "[ROLE_OWNER]" ? 
             <div className='container'>
                 <br></br>
-                <Link to={`${url}/add`} className="btn btn-primary">Dodaj grupu</Link>
+                <div>
+                    <Link to={`${url}/add`} className="btn btn-primary">Dodaj grupu</Link>
+                    <br/>
+                    <br/>
+                </div>
+
 
             <Switch>
                 <Route exact path={path}>
                 <div>
                     {/* {keyslist.map((k)=>(k))} */}
                     {groups.map((gr)=>
-                    <div>
-                        <div className="card">
-                            <div className="card-body">
-                                <p className="h5">{gr.name}</p>
-                                <p>Djelatnost: {gr.job.name}</p>
-                                <p>Voditelj: {gr.leader.name} {gr.leader.surname}</p>
-                                <span>Članovi: 
-                                    <ul>
-                                        {gr.members.map( (mem)=>
-                                            <li>{mem.name} {mem.surname}</li>                    
-                                        )}
-                                    </ul>
-                                </span>
-                                <button
-                                    className="btn btn-danger mb-5"
-                                    onClick={() => handleDelete(gr.id)}
-                                >Obriši</button>
+                        <div>
+                            <div className="card">
+                                <div className="card-body">
+                                    <p className="h5">{gr.name}</p>
+                                    <p>Djelatnost: {gr.job.name}</p>
+                                    <p>Voditelj: {gr.leader.name} {gr.leader.surname}</p>
+                                    <span>Članovi: 
+                                        <ul>
+                                            {gr.members.map( (mem)=>
+                                                <li>{mem.name} {mem.surname}</li>                    
+                                            )}
+                                        </ul>
+                                    </span>
+                                    <button
+                                        className="btn btn-danger mb-5"
+                                        onClick={() => handleDelete(gr.id)}
+                                    >Obriši</button>
                                 </div>
                             </div>
+                            <br/>
                         </div>
                     
                     )}
--- frontend/ui/src/components/MainPage.js
+++ frontend/ui/src/components/MainPage.js
@@ -7,20 +7,7 @@ const MainPage = () => {
         <div>
             <div className="align-top"></div>
             <div className="align-middle">
-            <video autoPlay loop muted
-            style={{
-                position:"absolute",
-                width:"100%",
-                left: "50%",
-                top: "50%",
-                height: "100%",
-                objectFit:"cover",
-                transform:"translate(-50%, -50%)",
-                zIndex:"-1"
-            }}
-            >
-                <source src={Backvid} type="video/mp4"/>
-            </video>
+            
                 <p className="text-center text-white h1">Radno vrijeme&copy; by Mi puno radimo </p>
             </div>
             
